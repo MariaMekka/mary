05b16dc1c61c9f8d182e93493da633c6baf3f876
Anton Klimenko <antonyklimenko@gmail.com>
2022-10-18 21:38:37 +0300

    task3.py

--- task3.py
+++ task3.py
@@ -1,22 +1,12 @@
+def season(month):
+    season = "spring"
+    if 2 >= month >= 1 or month == 12:
+        season = "winter"
+    elif 8 >= month >= 6:
+        season = "summer"
+    elif 11 >= month >= 9:
+        season = "autumn"
+    return season
 
-#empty_list = []
-#empty_list = list()
-
-#numbers = list(1, 2, 3, 4, 5, 6, 7, 8, 9)
-
-#print(numbers)
-
-#for element in numbers:
-#    print(element)
-
-numbers = [11, 221, 73, 65, 89, 18]
-
-numbers.insert(0, 100)
-numbers.append(1000)
-print(numbers)
-
-for index in range(0, len(numbers), 2):
-#    print(str(index + " - " + str(element[intex])))
-    print(f"{index} - {numbers[index]}")
 
 
