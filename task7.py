ace342539c98ee793861ee57aeb0b985259a9c06
Anton Klimenko <antonyklimenko@gmail.com>
2022-10-20 20:28:58 +0300

    task3.py

---
+++ task7.py
@@ -0,0 +1,14 @@
+import strong_password_task
+
+
+def main():
+    password = input("Input: ")
+
+    msg = strong_password_task.check_password(password)
+
+    msg =
+
+    print("You password is" + str(msg)
+
+if __name__ == "__main__":
+    main()
